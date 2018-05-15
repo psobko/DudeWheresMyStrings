6234508b8098394a7dcd44b811611df28370c994
Piotrek Sobkowski <peter@psobko.io>
2018-05-15 11:49:49 -0400

    Initial Commit

---
+++ DudeWheresMyStrings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DudeWheresMyStrings
+//
+//  Created by Peter Sobkowski on 2018-05-15.
+//  Copyright © 2018 psobko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DudeWheresMyStrings/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DudeWheresMyStrings
+//
+//  Created by Peter Sobkowski on 2018-05-15.
+//  Copyright © 2018 psobko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
